3991eecb01f278a759e4c8e46ab17346ca33acb4
Mehmet Efe <mehmetservanefe@gmail.com>
2021-08-17 01:15:46 +0300

    kosullar eklendi

--- deneme/src/com/company/Main.java
+++ deneme/src/com/company/Main.java
@@ -7,19 +7,29 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
+        System.out.print("Matematik notunu gir : ");
         int maths = sc.nextInt();
+        System.out.print("Fizik notunu gir : ");
         int physics = sc.nextInt();
+        System.out.print("Kimya notunu gir : ");
         int chemistry = sc.nextInt();
+        System.out.print("Turkce notunu gir : ");
         int literature = sc.nextInt();
+        System.out.print("Tarih notunu gir : ");
         int history = sc.nextInt();
+        System.out.print("Muzik notunu gir : ");
         int musics = sc.nextInt();
 
         double average = (maths+physics+chemistry+literature+history+musics)/6;
 
         System.out.println(average);
 
-        System.out.println(average/2);
+        if(average>60){
+            System.out.println("Sinifi gecti");
+        }
+        else{
+            System.out.println("Sinifta kaldi");
+        }
 
-        System.out.println(average*8);
     }
 }
