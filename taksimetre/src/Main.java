b968c855dad471c9de2e8c2d04d8d7fca25c2499
Mehmet Efe <mehmetservanefe@gmail.com>
2021-08-17 04:11:37 +0300

    kosullar eklendi

---
+++ taksimetre/src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Gidilen kilometreyi yaziniz: ");
+        double km = sc.nextInt();
+        double fatura = 10.0+(km*2.2);
+        if(fatura<20){
+            fatura = 20;
+        }
+        System.out.println("Odenecek tutar : "+fatura+" tldir.");
+    }
+}
